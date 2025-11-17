c285b0e4d4158964f8bf51445dcffd0a6d6c3a5f
ranjith2007 <ranjithkumar0799@gmail.com>
2025-11-17 18:20:34 +0530

    k8s

---
+++ install-kubectl.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#check whether root user is there or not
+R="\e[31m"
+N="\e[0m"
+
+
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.34.1/2025-09-19/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv kubectl /usr/local/bin/kubectl
+
+kubectl version
