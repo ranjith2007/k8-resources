6fd4d2b99e84adcb79cf93f7251208bb56f456b1
ranjith2007 <ranjithkumar0799@gmail.com>
2025-11-17 18:26:43 +0530

    k8s

---
+++ install-eksctl.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#check whether root user is there or not
+R="\e[31m"
+N="\e[0m"
+
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
